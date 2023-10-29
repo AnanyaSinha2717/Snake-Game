3928e9674a5dab70cc9800092c0b7689eda1d0ef
Replit user <>
2023-10-29 15:58:08 +0000

    reset high score to 0

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ bound.color('silver')
 bound.goto(-280, 280)
 bound.pd()
 for j in range(4):
-    bound.fd(560)
-    bound.right(90)
+  bound.fd(560)
+  bound.right(90)
 
 # calling classes
 snake = Snake()
@@ -35,28 +35,28 @@ screen.onkey(snake.right, "Right")
 
 game_on = True
 while game_on:
-    screen.update()
-    time.sleep(0.1)
-    snake.move()
+  screen.update()
+  time.sleep(0.1)
+  snake.move()
 
-    # detect collision with food
-    if snake.snake[0].distance(food) < 18:
-        food.refresh()
-        snake.extend()
-        score.score_calc()
+  # detect collision with food
+  if snake.snake[0].distance(food) < 18:
+    food.refresh()
+    snake.extend()
+    score.score_calc()
 
-    # detect collision with wall
-    if snake.snake[0].xcor() > 270 or snake.snake[0].xcor() < -270 or snake.snake[0].ycor() > 270 or snake.snake[0].ycor() < -270:
-        score.reset_board()
-        snake.reset_snake()
+  # detect collision with wall
+  if snake.snake[0].xcor() > 270 or snake.snake[0].xcor(
+  ) < -270 or snake.snake[0].ycor() > 270 or snake.snake[0].ycor() < -270:
+    score.reset_board()
+    snake.reset_snake()
 
-    # detect collision with tail
-    for seg in snake.snake[1:]:
-        if seg == snake.snake[0]:
-            pass
-        elif snake.snake[0].distance(seg) < 10:
-            score.reset_board()
-            snake.reset_snake()
-
+  # detect collision with tail
+  for seg in snake.snake[1:]:
+    if seg == snake.snake[0]:
+      pass
+    elif snake.snake[0].distance(seg) < 10:
+      score.reset_board()
+      snake.reset_snake()
 
 screen.exitonclick()
